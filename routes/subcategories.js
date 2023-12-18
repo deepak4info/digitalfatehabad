b390f6847462dfa83cf29bc4409ffa5d8bbb0792
DESKTOP-PUNHFLS\DIGI IT <dev4ajaykumar@gmail.com>
2023-12-18 19:37:23 +0530

    main

---
+++ routes/subcategories.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const subcategoryController = require("../controllers/subcategoryController");
+router.get(
+  "/:categoryId/subcategories",
+  subcategoryController.getAllSubcategories
+);
+
+// POST create a new subcategory for a specific category
+router.post(
+  "/:categoryId/subcategories",
+  // authMiddleware.verifyToken,
+  subcategoryController.createSubcategory
+);
+
+// PUT update a subcategory for a specific category
+router.put(
+  "/:categoryId/subcategories/:id",
+  // authMiddleware.verifyToken,
+  subcategoryController.updateSubcategory
+);
+
+// DELETE a subcategory for a specific category
+router.delete(
+  "/:categoryId/subcategories/:id",
+  // authMiddleware.verifyToken,
+  subcategoryController.deleteSubcategory
+);
+
+router.get(
+  "/sub/count",
+  // authMiddleware.verifyToken,
+  subcategoryController.getSubCategoriesCount
+);
+module.exports = router;
